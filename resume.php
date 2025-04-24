c3e7f86e75820c4ccfa37418c7b9237860c3cd20
wanyenze-clare <wanyenzeclawet@gmail.com>
2025-04-24 15:39:00 +0300

    profile-photo

---
+++ resume.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>
+	Riaz Resume
+</title>
+<link rel="stylesheet" type="text/css" href="resume-style.css">
+</head>
+<body>
+	<div class="profile-infocontainer">
+    <div class="profile-photo">
+  <img src="profile.png" alt="Eddy Riaz profile photo" class="profile-photo"></div>
+  <div class="profile-body">
+  <h1>Eddy Riaz</h1>
+  <p class="tile">Accountant</p>
+  <p class="contact-info">
+    <a href="mailto:riazeddy@gmail.com">riazeddy@gmail.com</a><br>
+    <a href="tel:+254768513236">+254 768 513 236"></a>
+</p>
+</div>
+</div>
+</div>
+<div>
+  <h2>My Education and Skills</h2>
+  <table>
+    <tr>
+      <th>Education</th>
+      <th>Skills</th>
+    </tr>
+    <tr>
+      <td>Web development</td>
+      <td>Coding</td>
+    </tr>
+  </table>
+</div>
+<form action="send-message.php" method="post">
+  <label>Your Name</label>
+  <input type="text" name="sender">
+  <label>Message</label>
+  <textarea name="message"></textarea>
+  <button type="submit">send</button>
+</form>
+</body>
+</html>
